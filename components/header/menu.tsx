83d7284cf51aa450b307beb40350a56e6fe4eed8
krsticzoran <zorankrstic81@gmail.com>
2025-12-19 19:27:36 +0100

    feat: add left menu image

--- components/header/menu.tsx
+++ components/header/menu.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import Link from "next/link";
+import Image from "next/image";
 
 import { getMenuLinks } from "@/data/menu-links";
 
@@ -33,29 +34,50 @@ export default function Menu() {
         />
       </button>
       <SheetContent
+        side="left"
+        className=" hidden md:block md:w-2/3 xl:w-3/4 [&>button]:hidden border-0!"
+      >
+        <SheetTitle className="sr-only">running image</SheetTitle>
+        <div className="relative h-full w-full">
+          <Image
+            src="/header-image.jpg"
+            fill
+            alt="running women"
+            className="object-cover"
+          />
+          <div
+            className="absolute inset-0"
+            style={{
+              background:
+                "linear-gradient(to left, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0) 25%)",
+            }}
+          />
+        </div>
+      </SheetContent>
+      <SheetContent
         side="right"
         className="w-full md:w-1/3 xl:w-1/4 bg-black text-white border-none p-10 [&>button]:hidden"
       >
         <SheetTitle className="sr-only">menu</SheetTitle>
-        <div className="absolute right-10 top-10">
+        <div className="absolute right-4 top-7">
           <button
             onClick={() => setIsMenuOpen((prev) => !prev)}
             className="relative h-2.5 w-8 flex flex-col justify-between cursor-pointer"
             aria-label="Toggle menu"
           >
-            <div className="relative h-4 w-8">
+            <div className="relative h-[26px] w-[26px]">
               <div className="absolute top-1/2 left-0 h-0.5 w-8 -translate-y-1/2 rotate-45 bg-white" />
               <div className="absolute top-1/2 left-0 h-0.5 w-8 -translate-y-1/2 -rotate-45 bg-white" />
             </div>
           </button>
         </div>
-        <nav className="flex h-full flex-col justify-center gap-10">
+        <nav className="flex h-full flex-col mt-[66px] gap-4">
           {menuLinks.map((item) => (
             <Link
               key={item.href}
               href={item.href}
               onClick={() => setIsMenuOpen(false)}
-              className="font-familjen text-4xl tracking-tight hover:opacity-70 transition"
+              className="font-familjen text-[22px] leading-[22px] font-medium opacity-75"
             >
               {item.label}
             </Link>
