0606bc0550fe9bd02cead66cd90e12474bdfdfdc
krsticzoran <zorankrstic81@gmail.com>
2025-12-31 08:31:49 +0100

    style:add list in snaga i felxibilnost

--- components/mdx/checklist.tsx
+++ components/mdx/checklist.tsx
@@ -1,6 +1,6 @@
 export function Checklist() {
   return (
-    <span className="text-custom-gray bg-black px-1.5 py-0.5 text-sm text-center mr-1 ">
+    <span className="text-custom-gray bg-black px-1.5 py-0.5 text-sm text-center mr-1">
       ✓
     </span>
   );
