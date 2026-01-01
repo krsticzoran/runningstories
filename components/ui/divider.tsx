b0ddd9ab78baf5da812209b0ad8d5de07b7b88cc
krsticzoran <zorankrstic81@gmail.com>
2026-01-01 09:06:26 +0100

    style: convert custom-dark from rgb to hex (#222222)

--- components/ui/divider.tsx
+++ components/ui/divider.tsx
@@ -6,7 +6,7 @@ export default function Divider({ className }: DividerProps) {
   return (
     <div className={`flex justify-center w-full my-[60px] ${className ?? ""}`}>
       <div className="max-w-[1300px] px-5 sm:px-8 lg:px-[60px] xl:px-0  w-full">
-        <div className="h-0.5 w-full bg-[#ddd]" />
+        <div className="h-0.5 w-full bg-custom-gray" />
       </div>
     </div>
   );
