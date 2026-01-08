0949042f8e932d754ed226f1a1dc813c6ac95111
krsticzoran <zorankrstic81@gmail.com>
2026-01-08 20:53:44 +0100

    feat:add link to section heading

--- components/home/HomeTraining.tsx
+++ components/home/HomeTraining.tsx
@@ -1,6 +1,7 @@
 import { CategoryCard } from "@/components/cards/categroy-card";
 import { posts } from "#site/content";
 import { Container } from "../layout/container";
+import Link from "next/link";
 
 export default function HomeTraining() {
   const categoryPosts = posts
@@ -11,9 +12,11 @@ export default function HomeTraining() {
   return (
     <section className="bg-bg-secondary py-[100px] xl:py-[120px]">
       <Container className="flex flex-col">
-        <h2 className="text-[40px] sm:text-[48px] xl:text-[56px] leading-tight text-black pb-3 uppercase font-medium mb-4 xl:mb-6">
-          Trening
-        </h2>
+        <Link href="trening">
+          <h2 className="text-[40px] sm:text-[48px] xl:text-[56px] leading-tight text-black pb-3 uppercase font-medium mb-4 xl:mb-6">
+            Trening
+          </h2>
+        </Link>
         <CategoryCard post={categoryPosts[0]} variant="featured" titleAs="h3" />
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-3 mt-3">
           {categoryPosts.slice(1).map((post) => (
