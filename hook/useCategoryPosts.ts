2f13b807c220241c715517acbf2c2e67f5589da2
krsticzoran <zorankrstic81@gmail.com>
2026-01-14 22:46:08 +0100

    feat: exclude featured posts from category hook

--- hook/useCategoryPosts.ts
+++ hook/useCategoryPosts.ts
@@ -2,7 +2,7 @@ import { posts } from "#site/content";
 
 export function useCategoryPosts(category: string, limit: number = 3) {
   const categoryPosts = posts
-    .filter((post) => post.category === category)
+    .filter((post) => post.category === category && post.featured !== true)
     .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
     .slice(0, limit);
 
