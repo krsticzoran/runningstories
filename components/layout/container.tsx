442f92f3cae106d17131abc2797156b9288217b4
krsticzoran <zorankrstic81@gmail.com>
2026-02-20 11:28:34 +0100

    feat: add 'as' prop to Container for flexible element type

--- components/layout/container.tsx
+++ components/layout/container.tsx
@@ -1,18 +1,23 @@
-import { ReactNode } from "react";
+import { ReactNode, ElementType } from "react";
 
 interface ContainerProps {
   children: ReactNode;
   className?: string;
+  as?: ElementType;
 }
 
-export function Container({ children, className }: ContainerProps) {
+export function Container({
+  children,
+  className,
+  as: Component = "div",
+}: ContainerProps) {
   return (
-    <div
+    <Component
       className={`px-5 sm:px-8 lg:px-[60px] xl:px-0 max-w-[1300px] w-full mx-auto ${
         className ?? ""
       }`}
     >
       {children}
-    </div>
+    </Component>
   );
 }
--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -1,9 +1,22 @@
+import { getMenuLinks } from "@/lib/menu";
+import { Container } from "../container";
+
 export function Footer() {
+  const links = getMenuLinks();
   return (
-    <footer className="bg-black py-6">
-      <div className="text-center text-sm text-white/60">
-        © {new Date().getFullYear()} Trkačke priče. Sva prava zadržana.
-      </div>
+    <footer className="bg-black ">
+      <Container>
+        <div>
+          {links.map((link) => (
+            <div key={link.label} className="text-white">
+              {link.label}
+            </div>
+          ))}
+        </div>
+        <div className="py-6 text-center text-sm text-white/60">
+          © {new Date().getFullYear()} Trkačke priče. Sva prava zadržana.
+        </div>
+      </Container>
     </footer>
   );
 }
