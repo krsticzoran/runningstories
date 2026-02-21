4663279f5289787c3c1305fce602b968a7175bd8
krsticzoran <zorankrstic81@gmail.com>
2026-02-21 19:48:40 +0100

    feat(blog): add Adidas Adizero Evo SL review article

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -1,18 +1,9 @@
-import { getMenuLinks } from "@/lib/menu";
 import { Container } from "../container";
 
 export function Footer() {
-  const links = getMenuLinks();
   return (
     <footer className="bg-black ">
       <Container>
-        <div>
-          {links.map((link) => (
-            <div key={link.label} className="text-white">
-              {link.label}
-            </div>
-          ))}
-        </div>
         <div className="py-6 text-center text-sm text-white/60">
           © {new Date().getFullYear()} Trkačke priče. Sva prava zadržana.
         </div>
